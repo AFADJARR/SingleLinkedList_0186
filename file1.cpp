4dfe35095e20b179cc710cc88830667d20c05959
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-08 10:09:21 +0700

    menambahkan logika untuk menyisipkan node

--- file1.cpp
+++ file1.cpp
@@ -27,6 +27,14 @@ public:
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
 
-        if
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        
+        }
     }
 }
