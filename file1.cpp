317de083d3bca8ad800436d92cab2eca2c4b62fd
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-08 10:46:44 +0700

    menambahkan fungsi menghapus node

--- file1.cpp
+++ file1.cpp
@@ -72,7 +72,30 @@ public:
             *current =(*current)->next;
         }
 
-        return (*current != NULL)
+        return (*current != NULL);
+    }
 
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (Search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+        START = START->next;
+        else
+        previous->next = current->next;
+
+        delete current;
+        return true;
+
+    }
+
+    void traverse()
+    {
+        if(listEmpty())
+        {
+
+        }
     }
 };
