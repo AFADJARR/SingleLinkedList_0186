cd60b1ba37e65248e204968777a6d8af5f49a656
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-08 11:00:50 +0700

    menaambahkan interaktif dlam fungsi main

--- file1.cpp
+++ file1.cpp
@@ -110,3 +110,28 @@ public:
         }
     }
 };
+
+int main()
+{
+    SingleLinkedList mhs;
+    int nim;
+    char ch;
+    while(1)
+    {
+        cout << endl
+            <<"menu";
+            cout << endl
+                 << "1, Menambahkan data kedalam list" << endl;
+            cout << "2, menghapus data dari dalam list" << endl;
+            cout << "3, menampilkan semua data dalam list" << endl;
+            cout << "4, mencari data dalam list" << endl;
+            cout << "5, keluar" << endl;
+            cout << endl
+                 << "masukan pilihan (1-5):";
+            cin >> ch;
+            switch (ch)    
+            cin >> ch;
+                        
+    }
+    }
+}
